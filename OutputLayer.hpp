6eda17f77b4540803d47633bb6a4796a8fffc159
ChachouCarter <114995353+ChachouCarter@users.noreply.github.com>
2023-05-10 11:42:36 +0200

    Update OutputLayer.hpp

--- OutputLayer.hpp
+++ OutputLayer.hpp
@@ -3,6 +3,8 @@
 
 #include "Layer.hpp"
 #include "TrainingFunction.hpp"
+#include "Matrix.hpp"
+
 
 #include <vector>
 using std::vector;
@@ -12,22 +14,29 @@ class OutputLayer : public Layer
 private:
     //! Sortie du reseau
     int result;
-    vector<double> Proba;
+    Matrix Proba;
     int tailleInput;
     int tailleOutput;
 
 public:
     //! Constructeur
-    OutputLayer();
-    //! Creer la couche de sortie
-    void creer_derniere_couche(int tailleInput, int tailleOutput, TrainingFunction f);
-    //! Prend en parametre une fonction correspondant a la fontion de perte du reseau, ici, l entropie croise
-    void fonction_de_perte();
-    //! Enregistre le resultat dans l attribut Proba
-    void enregistrer_vecteur_de_probabilite(Layer L);
-    void obtenir_resultat();
+    OutputLayer(int Nbe, int Nbs);
     //! Destructeur
     ~OutputLayer();
+
+    //! Creer la couche de sortie
+    //void creer_derniere_couche(int tailleInput, int tailleOutput, TrainingFunction f);
+    //! Prend en parametre une fonction correspondant a la fontion de perte du reseau, ici, l entropie croise
+    //void //fonction_de_perte();
+    //! Enregistre le resultat dans l attribut Proba
+    
+    Matrix getProba();
+    Matrix calculer_vecteur_de_probabilite();
+    Matrix getProba();
+    Matrix setProba(Matrix P);
+    void obtenir_resultat();
+    void calculerDelta(Layer L);
+     
 };
 
 #endif
